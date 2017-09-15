0ca9fe94d6dd3b866c6750a760f1ac95a2528a21
gogi.strat <gogi.strat@gmail.com>
2017-09-15 08:03:56 -0400

    Gmail Done.Trying from  iTSS server

--- mailer.js
+++ mailer.js
@@ -1,42 +1,46 @@
 var nodemailer = require('nodemailer');
-/*
+
 var transporter = nodemailer.createTransport({
-    service: 'gmail',
+  service: 'gmail',
   //  secure: true,
   //  port: 587,
-    auth: {
-            type: 'OAuth2',
-            user: 'itss.softwares@gmail.com',
-            clientId: '390578102235-79h9f88fao8rbs8rfbf52m2o04son28m.apps.googleusercontent.com',
-            clientSecret: 'cUf2t9y46h3wFmX7J1WU6DfY',
-            refreshToken: '1/ioctyZrlb6pzKMTArz2fRRva58b3rx7m1Rb8gPTfwzQPBUDyT4t5WmmtqOUrh_XE'
-   
-    }
+  auth: {
+    type: 'OAuth2',
+    user: 'itss.softwares@gmail.com',
+    clientId: '390578102235-79h9f88fao8rbs8rfbf52m2o04son28m.apps.googleusercontent.com',
+    clientSecret: 'cUf2t9y46h3wFmX7J1WU6DfY',
+    refreshToken: '1/ioctyZrlb6pzKMTArz2fRRva58b3rx7m1Rb8gPTfwzQPBUDyT4t5WmmtqOUrh_XE'
+
+  }
+});
+
+
+
+/*
+var transporter = nodemailer.createTransport({
+     
+       host : 'mail.itransformsolutions.com',
+      // port :587,
+       //secure: false, 
+       auth: {         
+                user: 'system@itransformsolutions.com',
+                pass: 'abc123ABC'    
+             }
 });
 */
 
 
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-       host: '',
-       port: 552,
-       auth: {         
-                user: 'itss.softwares@gmail.com',
-                pass: ''    
-             }
-});
-
-
 var mailOptions = {
-    from: 'newmailer@newcomp.com',
-    to: 'sujitthombare01@gmail.com',
-    subject: 'Nodemailer test 1',
-    html: '<h1>Hello World!!!!</h1><br> <h3>This is first email</h3>'
+  from: 'system@itransformsolutions.com',
+  to: 'sujitthombare01@gmail.com',
+  subject: 'Nodemailer test 1',
+  html: '<h1>Hello World!!!!</h1><br> <h3>This is first email</h3>'
 }
 
-transporter.sendMail(mailOptions, function (err, res) {
-    if(err){
-        console.log(err);
-    } else {
-        console.log('Email Sent');
-    }});   
+transporter.sendMail(mailOptions, function(err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Email Sent');
+  }
+});
