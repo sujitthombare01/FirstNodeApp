42ead1a837d5bf4467fe3e257aa891627b9e0461
gogi.strat <gogi.strat@gmail.com>
2017-09-19 06:48:22 -0400

    Mail using gmail and itss server

--- mailer.js
+++ mailer.js
@@ -1,5 +1,7 @@
 var nodemailer = require('nodemailer');
 
+
+/*  /////using gmail 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   //  secure: true,
@@ -8,31 +10,36 @@ var transporter = nodemailer.createTransport({
     type: 'OAuth2',
     user: 'itss.softwares@gmail.com',
     clientId: '390578102235-79h9f88fao8rbs8rfbf52m2o04son28m.apps.googleusercontent.com',
-    clientSecret: 'cUf2t9y46h3wFmX7J1WU6DfY',
+    clientSecret: 'cUf2t9y46h3wFmX7J1WU6DfYq',
     refreshToken: '1/ioctyZrlb6pzKMTArz2fRRva58b3rx7m1Rb8gPTfwzQPBUDyT4t5WmmtqOUrh_XE'
 
   }
 });
+*/
 
 
 
-/*
+
+
 var transporter = nodemailer.createTransport({
      
        host : 'mail.itransformsolutions.com',
-      // port :587,
+       port :587,
        //secure: false, 
        auth: {         
                 user: 'system@itransformsolutions.com',
                 pass: 'abc123ABC'    
-             }
+             },
+   tls: {
+        rejectUnauthorized:false
+    }
 });
-*/
+
 
 
 var mailOptions = {
   from: 'system@itransformsolutions.com',
-  to: 'sujitthombare01@gmail.com',
+  to: 'kedarlachke1@gmail.com',
   subject: 'Nodemailer test 1',
   html: '<h1>Hello World!!!!</h1><br> <h3>This is first email</h3>'
 }
@@ -41,6 +48,7 @@ transporter.sendMail(mailOptions, function(err, res) {
   if (err) {
     console.log(err);
   } else {
+     console.log(res);
     console.log('Email Sent');
   }
 });
